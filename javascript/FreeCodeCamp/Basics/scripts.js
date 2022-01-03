627dcc8684623f60cfbd5c4b05bf813f954626ef
Dipankar <FromLinuxBox>
2022-01-03 15:18:40 +0530

    PATCH 2: Completed the variables part of Javascript

--- javascript/FreeCodeCamp/Basics/scripts.js
+++ javascript/FreeCodeCamp/Basics/scripts.js
@@ -13,3 +13,19 @@ c="I am a";
 a = a + 1;
 b = b + 5;
 c = c + " String!";
+
+// If you replace var with let in the code above, it results in an error:
+
+let camper = "James";
+let camper = "David";
+/**
+ * The error can be seen in your browser console.
+ * So unlike var, when you use let, a variable with the same name can only be declared once.
+ */
+const PIE=3.18456;
+console.log(PIE, camper);
+
+/**
+ * there is 
+ * += , -=, *= , /= and the ++ , --
+ */
