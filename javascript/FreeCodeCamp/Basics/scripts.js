089c3e73cabc43f3e3ab47955c943e6648abbf94
Dipankar <FromLinuxBox>
2022-01-03 12:17:29 +0530

    PATCH completed the variables in javascript

--- javascript/FreeCodeCamp/Basics/scripts.js
+++ javascript/FreeCodeCamp/Basics/scripts.js
@@ -1,1 +1,15 @@
 console.log("Hello\n");
+
+var a;
+var b;
+var c;
+// a=5;
+/**
+ * Initialize the three variables a, b, and c with 5, 10, and "I am a" respectively 
+ * so that they will not be undefined.
+ */
+b=10;
+c="I am a";
+a = a + 1;
+b = b + 5;
+c = c + " String!";
