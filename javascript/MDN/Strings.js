5b967d366ff12054b3f1d50e2e3c39bd83ddda16
DipankarDas <dipsonu10@hotmail.com>
2022-01-20 23:48:44 +0530

    Completed the MDN text part String JS

---
+++ javascript/MDN/Strings.js
@@ -0,0 +1,21 @@
+const str1 = "Welcome";
+const str2 = "to the";
+const res = `${str1} ${str2} MATRIX`;
+console.log(res);
+
+
+let number = 32;
+console.log(typeof number);
+let N = number.toString();
+
+console.log(typeof N);
+console.log(typeof Number(N));
+
+// multiple line strings
+const multiLine = `Hey, I am Dipankar
+nice to see you
+currently learning
+* javascript
+* golang
+* docker & kubernetes`;
+console.log(multiLine);
