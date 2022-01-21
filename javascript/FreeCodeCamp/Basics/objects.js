3148efa7f572d3459ad9b59d038f921c37705dc8
DipankarDas <dipsonu10@hotmail.com>
2022-01-21 23:16:09 +0530

    Completed the objects

--- javascript/FreeCodeCamp/Basics/objects.js
+++ javascript/FreeCodeCamp/Basics/objects.js
@@ -2,15 +2,15 @@ const myDog = {
     name: "Metu",
     tails: 6,
     legs: 2,
-    // 2: 234, the numbers can also be properties as JS typecase it to 
+    // 2: 234, the numbers can also be properties as JS typecase it to
     // string automatically
     friends: ['Meet', 'hahah']
 };
 /**
  * @property of accessing @class
- * There are two ways to access the properties of an object: 
+ * There are two ways to access the properties of an object:
  * dot notation (.) and bracket notation ([]), similar to an array.
- * Dot notation is what you use when you know the name of 
+ * Dot notation is what you use when you know the name of
  * the property you're trying to access ahead of time.
  */
 console.log(myDog.friends);
@@ -39,7 +39,7 @@ console.log(myDog);
 
 // use it as a dictonary but of fixed key value pair
 function hello(val) {
-    
+
     const Class = {
         "alpha": "Adams",
         "bravo": "Boston",
@@ -62,3 +62,40 @@ const myObj = {
 };
 console.log(myObj.hasOwnProperty("top"));
 console.log(myObj.hasOwnProperty("hello"));
+// Setup
+var recordCollection = {
+    2548: {
+        albumTitle: 'Slippery When Wet',
+        artist: 'Bon Jovi',
+        tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+        albumTitle: '1999',
+        artist: 'Prince',
+        tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+        artist: 'Robert Palmer',
+        tracks: []
+    },
+    5439: {
+        albumTitle: 'ABBA Gold'
+    }
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+    if (prop !== 'tracks' && value !== "") {
+        records[id][prop] = value;
+    } else if (prop === "tracks" && records[id].hasOwnProperty("tracks") === false) {
+        records[id][prop] = [value];
+    } else if (prop === "tracks" && value !== "") {
+        records[id][prop].push(value);
+    } else if (value === "") {
+        delete records[id][prop];
+    }
+    return records;
+}
+
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+console.log(recordCollection);
