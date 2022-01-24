25fb911befbc7b705e1b1d38f96824c1d05a4a8a
DipankarDas <dipsonu10@hotmail.com>
2022-01-25 00:13:01 +0530

    Completed the first Step in JS MDN

--- javascript/FreeCodeCamp/DSA/queue.js
+++ javascript/FreeCodeCamp/DSA/queue.js
@@ -3,18 +3,29 @@ const que = [1, 2, 3, 24, 6, 7, 7, 8];
 
 __main__();
 
-function __push(a) {
-    que.push(a);
-}
+class Queue { 
+    constructor() {
+        let que = [];
+    }
 
-function __pop() {
-    return que.shift();
-}
+    __push(num) {
+        que.push(num);
+    }
 
+    __peek() {
+        return que[0];
+    }
+
+    __pop() {
+        que.pop();
+    }
+};
 
 function __main__() {
-    console.log("Queue ", que);
-    __push(1232);
-    console.log("Queue ",que);
-    console.log("Popped element: ", __pop());
+    // console.log("Queue ", que);
+    // __push(1232);
+    // console.log("Queue ",que);
+    // console.log("Popped element: ", __pop());
+
+    
 }
---
+++ javascript/MDN/Assign1/main.js
@@ -0,0 +1,52 @@
+/**
+ * 1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
+*/
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array) {
+  const random = Math.floor(Math.random() * array.length);
+  return array[random];
+}
+/*
+2. RAW TEXT STRINGS
+*/
+const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
+
+const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+
+const insertY = ["the soup kitchen", "Disneyland", "the White House"]
+
+const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"]
+/*
+3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
+*/
+randomize.addEventListener('click', result);
+
+function result() {
+
+  let newStory = storyText;
+  const xItem = randomValueFromArray(insertX);
+  const yItem = randomValueFromArray(insertY);
+  const zItem = randomValueFromArray(insertZ);
+  newStory = newStory.replace(":insertx:", xItem);
+  newStory = newStory.replace(":inserty:", yItem);
+  newStory = newStory.replace(":insertz:", zItem);
+  newStory = newStory.replace(":insertx:", xItem);
+  if(customName.value !== '') {
+    const name = customName.value;
+    newStory = newStory.replace('Bob', name);
+  }
+  console.log(newStory);
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(136.0777);
+    const temperature =  Math.round(34.44444);
+    newStory = newStory.replace("300 pounds", `${weight} stone`);
+    newStory = newStory.replace("94 fahrenheit", `${temperature} centigrade`);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
