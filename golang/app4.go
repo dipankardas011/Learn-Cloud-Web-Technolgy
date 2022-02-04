49a7e9578c843aee36064b22db9ed5376d208fac
Dipankar Das <dipsonu10@hotmail.com>
2022-02-05 00:23:35 +0530

    Some more use of switch case with time() GOLANG

--- golang/app4.go
+++ golang/app4.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"time"
 )
 
 const (
@@ -14,6 +15,29 @@ func needInt(x int) int { return x*10 + 1 }
 
 func needFloat(x float64) float64 {
 	return x * 0.1
+}
+
+func timming() {
+	fmt.Println("time f()")
+
+	today := time.Now().Weekday()
+	unixTime := time.Now().UnixNano()
+
+	fmt.Println(today)
+	fmt.Println(unixTime)
+
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today is saturday")
+	case today + 1:
+		fmt.Println("Tomorrow is saturday")
+	case today + 2:
+		fmt.Println("In 2 days")
+	case today - 1:
+		fmt.Println("Yesterday was saturday")
+	default:
+		fmt.Println("too far")
+	}
 }
 
 func main() {
@@ -33,4 +57,5 @@ func main() {
 	fmt.Printf("%2.5f %T\n", PIE, PIE)
 	fmt.Println(needInt(SMALL))
 	fmt.Println(needFloat(SMALL))
+	timming()
 }
